73c8d20b2d516e8e080ba50670b70d49099e9ed6
Tabatha Poonpurmsiri <tabatha.pp@gmail.com>
2019-09-26 11:55:40 -0400

    updated orm.js

--- config/orm.js
+++ config/orm.js
@@ -1,2 +1,11 @@
 var connection = require("./connection.js");
 
+var orm = {
+    select: function(whatToSelect, tableInput) {
+      var queryString = "SELECT ?? FROM ??";
+      connection.query(queryString, [whatToSelect, tableInput], function(err, result) {
+        if (err) throw err;
+        console.log(result);
+      });
+    }
+}; // closing curly for orm var //
